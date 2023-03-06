5656b0834416aa04f745247370eca7172d53bf3d
mauripavan <mauriciopavan60@gmail.com>
2023-03-06 13:16:27 -0300

    feat: prettiern, eslint & babel config

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>F1 Prode!</Text>
       <StatusBar style="auto" />
     </View>
   );
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+      presets: ['babel-preset-expo'],
+      plugins: [
+          'relay',
+          'react-native-reanimated/plugin',
+          'module:react-native-dotenv',
+      ],
   };
 };
