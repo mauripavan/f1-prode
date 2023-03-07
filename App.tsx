8fd90ca955629d08e092129f6f01bd85a3e65537
mauripavan <mauriciopavan60@gmail.com>
2023-03-07 10:44:11 -0300

    feat: architecture definition

--- App.tsx
+++ App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>F1 Prode!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
