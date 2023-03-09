9f5131c566bc420d3315f499cec28dfa99727713
mauripavan <mauriciopavan60@gmail.com>
2023-03-09 10:19:46 -0300

    feat: custom button component

---
+++ src/components/CustomButton/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {ButtonContainer, ButtonText} from './styles';
+import {ButtonVariants, IButtonProps} from './types';
+
+const CustomButton = (props: IButtonProps) => {
+  const {
+    variant = ButtonVariants.Primary,
+    text,
+    fontColor,
+    style,
+    ...defaultProps
+  } = props;
+  return (
+    <ButtonContainer variant={variant} style={style} {...defaultProps}>
+      <ButtonText testID={'buttonText'} fontColor={fontColor} variant={variant}>
+        {text}
+      </ButtonText>
+    </ButtonContainer>
+  );
+};
+
+export default CustomButton;
---
+++ src/components/CustomButton/styles.ts
@@ -0,0 +1,81 @@
+import {Text, TouchableOpacity} from 'react-native';
+import styled from 'styled-components';
+
+import {fontPixel, heightPixel} from '../../constants/metrics';
+import {ButtonVariants, IButtonProps} from './types';
+
+export const ButtonContainer = styled(TouchableOpacity)<IButtonProps>`
+  width: 100%;
+  height: ${heightPixel(58)}px;
+  border-radius: 68px;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 12px 42px rgba(24, 39, 75, 0.12);
+  ${({variant, theme}) => {
+    switch (variant) {
+      case ButtonVariants.Primary:
+        return `
+          background-color: ${theme.colors.red[1]};
+          `;
+      case ButtonVariants.Secondary:
+        return `
+          background-color: ${theme.colors.white};
+          border-color: ${theme.colors.green[1]};
+          border-width: 1px;
+          `;
+      case ButtonVariants.Light:
+        return `
+          background-color: ${theme.colors.yellow[1]};
+          `;
+      case ButtonVariants.Dark:
+        return `
+          background-color:${theme.colors.gray[0]};
+          `;
+      case ButtonVariants.Disabled:
+        return `
+          background-color: ${theme.colors.green[1]};
+          `;
+    }
+  }};
+`;
+export const ButtonText = styled(Text)<IButtonProps>`
+  ${({variant, fontSize, fontWeight, fontColor, theme}) => {
+    switch (variant) {
+      case ButtonVariants.Primary:
+        return `
+          font-size: ${fontSize ? fontPixel(fontSize) : fontPixel(20)}px;
+          font-weight: ${fontWeight ? fontWeight : 700};
+          color:${fontColor ? fontColor : theme.colors.white};
+          font-family: 'MontserratSB'
+          `;
+      case ButtonVariants.Secondary:
+        return `
+          font-size: ${fontSize ? fontPixel(fontSize) : fontPixel(16)}px;
+          font-weight: ${fontWeight ? fontWeight : 600};
+          color: ${fontColor ? fontColor : theme.colors.green[1]};
+          font-family: 'MontserratSB'
+          `;
+      case ButtonVariants.Light:
+        return `
+          font-size: ${fontSize ? fontPixel(fontSize) : fontPixel(20)}px;
+          font-weight: ${fontWeight ? fontWeight : 700};
+          color: ${fontColor ? fontColor : theme.colors.green[1]};
+          font-family: 'MontserratSB'
+          `;
+      case ButtonVariants.Dark:
+        return `
+          font-size: ${fontSize ? fontPixel(fontSize) : fontPixel(20)}px;
+          font-weight: ${fontWeight ? fontWeight : 700};
+          color: ${fontColor ? fontColor : theme.colors.white};
+          font-family: 'MontserratSB'
+          `;
+      case ButtonVariants.Disabled:
+        return `
+          font-size: ${fontSize ? fontPixel(fontSize) : fontPixel(20)}px;
+          font-weight: ${fontWeight ? fontWeight : 700};
+          color: ${fontColor ? fontColor : theme.colors.white};
+          font-family: 'MontserratSB'
+          `;
+    }
+  }};
+`;
