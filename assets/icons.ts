a8d2bfd01d068e681898a9cc5c7dfc6ba521ce93
mauripavan <mauriciopavan60@gmail.com>
2023-03-15 16:59:26 -0300

    feat: adding icons

--- assets/icons.ts
+++ assets/icons.ts
@@ -2,8 +2,12 @@
 
 const locked = require('./icons/lock.png');
 const unlocked = require('./icons/unlock.png');
+const menu = require('./icons/menu.png');
+const home = require('./icons/home.png');
 
 export const icons = {
   locked,
   unlocked,
+  menu,
+  home
 };
