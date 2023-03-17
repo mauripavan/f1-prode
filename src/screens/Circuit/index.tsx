41b23b20ffb9865c7e497882c6d4692cb15d6318
mauripavan <mauriciopavan60@gmail.com>
2023-03-17 13:05:02 -0300

    feat: WIP Circuit screen

--- src/screens/Circuit/index.tsx
+++ src/screens/Circuit/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Platform, StatusBar, SafeAreaView} from 'react-native';
+import {useTheme} from 'styled-components';
+
+import {TextHighSpeed} from '../../components/Typography';
 
 const Circuit = () => {
+  const {colors} = useTheme();
   return (
-    <View>
-      <Text>Cirtcuit Screen</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        backgroundColor: colors.black,
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+      }}
+    >
+      <TextHighSpeed color={colors.white}>F1 Prode</TextHighSpeed>
+    </SafeAreaView>
   );
 };
 
