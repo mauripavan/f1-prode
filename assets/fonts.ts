b60cd926c99ed83a1525842fc42a83bf554f0b62
mauripavan <mauriciopavan60@gmail.com>
2023-03-17 17:23:19 -0300

    feat: adding new fonts

--- assets/fonts.ts
+++ assets/fonts.ts
@@ -5,6 +5,8 @@ const MontserratEL = require('./fonts/Montserrat-ExtraLight.otf');
 const MontserratL = require('./fonts/Montserrat-Light.otf');
 const MontserratR = require('./fonts/Montserrat-Regular.otf');
 const MontserratSB = require('./fonts/Montserrat-SemiBold.otf');
+const Formula1B = require('./fonts/Formula1-Bold_web_0.ttf');
+const Formula1R = require('./fonts/Formula1-Regular_web_0.ttf');
 
 export const fonts = {
   HighSpeed,
@@ -12,4 +14,6 @@ export const fonts = {
   MontserratL,
   MontserratR,
   MontserratSB,
+  Formula1B,
+  Formula1R,
 };
