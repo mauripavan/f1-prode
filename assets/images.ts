fceff8b4bfe0713c18818baad8dcec84ecca6146
mauripavan <mauriciopavan60@gmail.com>
2023-03-17 17:22:59 -0300

    feat: adding circuits images

--- assets/images.ts
+++ assets/images.ts
@@ -1,7 +1,31 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 
 const f1Logo = require('./images/f1-logo.png');
+const saudiCircuit = require('./images/saudi-circuit.png');
+const abudhabiCircuit = require('./images/abudhabi-circuit.png');
+const australiaCircuit = require('./images/australia-circuit.png');
+const austriaCircuit = require('./images/austria-circuit.png');
+const azerbaijanCircuit = require('./images/azerbaijan-circuit.png');
+const belgiumCircuit = require('./images/belgium-circuit.png');
+const dutchCircuit = require('./images/dutch-circuit.png');
+const emiliaCircuit = require('./images/emiliaromagna-circuit.png');
+const hungaryCircuit = require('./images/hungary-circuit.png');
+const miamiCircuit = require('./images/miami-circuit.png');
+const monacoCircuit = require('./images/monaco-circuit.png');
+const usCircuit = require('./images/us-circuit.png');
 
 export const images = {
   f1Logo,
+  saudiCircuit,
+  abudhabiCircuit,
+  australiaCircuit,
+  austriaCircuit,
+  azerbaijanCircuit,
+  belgiumCircuit,
+  dutchCircuit,
+  emiliaCircuit,
+  hungaryCircuit,
+  miamiCircuit,
+  monacoCircuit,
+  usCircuit,
 };
