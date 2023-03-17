d4e02b6a9452e07b0d5c43a7db97e031da2c24c0
mauripavan <mauriciopavan60@gmail.com>
2023-03-17 17:24:06 -0300

    feat: adding back icon

--- assets/icons.ts
+++ assets/icons.ts
@@ -4,10 +4,12 @@ const locked = require('./icons/lock.png');
 const unlocked = require('./icons/unlock.png');
 const menu = require('./icons/menu.png');
 const home = require('./icons/home.png');
+const back = require('./icons/left-arrow.png');
 
 export const icons = {
   locked,
   unlocked,
   menu,
   home,
+  back,
 };
