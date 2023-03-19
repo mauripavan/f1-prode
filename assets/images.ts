732e4ace7123ba20f90e5b86369f5261addb4685
mauripavan <mauriciopavan60@gmail.com>
2023-03-19 10:41:55 -0300

    feat: adding extra circuit images

--- assets/images.ts
+++ assets/images.ts
@@ -13,6 +13,15 @@ const hungaryCircuit = require('./images/hungary-circuit.png');
 const miamiCircuit = require('./images/miami-circuit.png');
 const monacoCircuit = require('./images/monaco-circuit.png');
 const usCircuit = require('./images/us-circuit.png');
+const italyCircuit = require('./images/italy-circuit.png');
+const japanCircuit = require('./images/japan-circuit.png');
+const lasVegasCircuit = require('./images/lasvegas-circuit.png');
+const mexicoCircuit = require('./images/mexico-circuit.png');
+const qatarCircuit = require('./images/qatar-circuit.png');
+const singaporeCircuit = require('./images/singapore-circuit.png');
+const brazilCircuit = require('./images/brazil-circuit.png');
+const britishCircuit = require('./images/british-circuit.png');
+const canadianCircuit = require('./images/canadian-circuit.png');
 
 export const images = {
   f1Logo,
@@ -28,4 +37,13 @@ export const images = {
   miamiCircuit,
   monacoCircuit,
   usCircuit,
+  italyCircuit,
+  japanCircuit,
+  lasVegasCircuit,
+  mexicoCircuit,
+  qatarCircuit,
+  singaporeCircuit,
+  brazilCircuit,
+  britishCircuit,
+  canadianCircuit
 };
